204bdb8fb683acdc761e2fcc94d0b385af9a5ac7
Danu Renmi Wijaya <dremiceki@gmail.com>
2025-12-24 20:49:31 +0700

    feat: add score between players

--- Rock-Papper-Scissors/rock-papper-scissors.js
+++ Rock-Papper-Scissors/rock-papper-scissors.js
@@ -1,7 +1,22 @@
 const choices = ['rock', 'paper', 'scissors'];
+
 const playerDisplay = document.getElementById('playerDisplay');
 const computerDisplay = document.getElementById('computerDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const winnerDisplay = document.getElementById('winnerDisplay');
+const gameButtons = document.querySelectorAll('button');
+const scoreDisplay = document.getElementById('scoreDisplay');
+
+rockButton.addEventListener('click', () => playGames('rock'));
+paperButton.addEventListener('click', () => playGames('paper'));
+scissorsButton.addEventListener('click', () => playGames('scissors'));
+
+let playerScore = 0;
+let computerScore = 0;
+const winningScore = 3;
 
 function playGames(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -22,8 +37,33 @@ function playGames(playerChoice) {
                 break;
         }
     }
+    if (result === 'YOU WIN!') {
+        playerScore++;
+    } else if (result === 'YOU LOSE!') {
+        computerScore++;
+    }
+    
     playerDisplay.textContent = `PLAYER : ${playerChoice}`;
     computerDisplay.textContent = `COMPUTER : ${computerChoice}`;
     resultDisplay.textContent = result;
-
+    scoreDisplay.textContent = `Score: Player - ${playerScore}, Computer - ${computerScore}`;
+    
+    resultDisplay.classList.toggle('resultStyleWin', result === 'YOU WIN!');
+    resultDisplay.classList.toggle('resultStyleLose', result === 'YOU LOSE!');
+    
+    checkGameWinner();
+}
+function checkGameWinner() {
+    if (playerScore === winningScore) {
+        winnerDisplay.textContent = 'Congratulations! You are the overall winner!';
+        disableButton();
+    } else if (computerScore === winningScore) {
+        winnerDisplay.textContent = 'Computer wins the game! Better luck next time!';
+        disableButton();
+    }
+}
+function disableButton() {
+    gameButtons.forEach(button => {
+        button.disabled = true;
+    });
 }
